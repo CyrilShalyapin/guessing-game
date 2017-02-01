303e908e26e62d15a42f66a14b4bb01d08d1ec1b
Cyril Shalyapin <cyrilshalyapin@gmail.com>
2017-02-02 00:04:04 +0300

    guessing-game/feature: empty fields filled

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.setMax = null;
+    	this.setMin = null;
+    }
 
     setRange(min, max) {
+    	this.setMax = max;
+    	this.setMin = min;
 
+    	return this;
     }
 
     guess() {
+    	let result = this.setMax + this.setMin;
+    	result /= 2;
+    	result = Math.round(result);
 
+    	return result;
     }
 
     lower() {
+    	this.setMax = this.guess();
 
+    	return this;
     }
 
     greater() {
+    	this.setMin = this.guess();
 
+    	return this;
     }
 }
 
